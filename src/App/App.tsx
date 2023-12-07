902fd3a06ab19aea15c5d43eb66617857c302719
Ankur Seth <iankurseth@gmail.com>
2023-12-07 18:37:30 +0530

    Added KEYS with keynames and coordinates

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,25 @@
+import React from 'react';
 import './App.css';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import Main from './Main/Main';
+import { KEYS } from './keyboard';
 
 
 
 export default function App () {
+	const handleKeydown = (ev: KeyboardEvent) => {
+		const date = new Date();
+		const key = KEYS.find(key => key.keynames.includes(ev.key));
+		if (key) {
+			console.log(`You pressed '${ev.key}' [${key.row}, ${key.column}] at ${date.toLocaleTimeString()}`);
+		}
+	};
+
+	React.useEffect(() => {
+		window.addEventListener('keydown', handleKeydown);
+		return () => window.removeEventListener('keydown', handleKeydown);
+	}, []);
 
 	return (
 		<div className="min-h-screen flex flex-col">

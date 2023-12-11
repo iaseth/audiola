812453be4050c7e03dc38d47c6e3748cab13b4c7
Ankur Seth <iankurseth@gmail.com>
2023-12-11 15:59:37 +0530

    Now using keyxy to get key coords

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,18 +3,18 @@ import './App.css';
 import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import Main from './Main/Main';
-import { KEYS } from './keyboard';
 import { playTone } from './music';
+import { getKeyDetails } from 'keyxy';
 
 
 
 export default function App () {
 	const handleKeydown = (ev: KeyboardEvent) => {
-		const key = KEYS.find(key => key.keynames.includes(ev.key));
+		const key = getKeyDetails(ev.key);
 
 		if (key) {
 			const {row, column} = key;
-			const freq = 200 + (row * 100) + (column * 5);
+			const freq = 100 + (row * 100) + (column * 5);
 			console.log(`You pressed '${ev.key}' [${row}, ${column}] at ${new Date().toLocaleTimeString()}`);
 			console.log(`Playing sound at ${freq}Hz`);
 			playTone(freq);
